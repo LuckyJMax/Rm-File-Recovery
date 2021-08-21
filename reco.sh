b2555f255686d85256e118abf6e02345efa5985a
Unknown <82444721+LuckyJMax@users.noreply.github.com>
2021-08-21 20:52:35 +0530

    Add files via upload

---
+++ reco.sh
@@ -0,0 +1,17 @@
+{
+echo -en 'Enter recover file name and extension : '
+read Re
+rm -rf Reco.sh
+cd
+cd /storage
+cd emulated
+cd 0
+cd Android
+cd media
+cd Del
+cp $Re /data/data/com.termux/files/home/Rm-File-Recovery/recoverfile
+cd
+clear
+}
+cd
+
